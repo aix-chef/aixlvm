b9002f7566affd655768431d2e3bd3b1473e2d16
Laurent GAY <laurent.gay@atos.net>
2016-04-12 18:29:15 +0200

    create

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'aixlvm'
+maintainer       '???'
+maintainer_email '???'
+license          '???'
+description      'LVM for AIX systems'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+source_url       '???'
+issues_url       '???'
+
+supports 'aix', '>= 6.1'
